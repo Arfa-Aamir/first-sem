e23d87fde21677d3ee9f4acfdb3e655ae12e4911
Arfa-Aamir <arfaamir29@gmail.com>
2023-09-15 00:31:25 -0700

    Demo of update

--- spacecraft.cpp
+++ spacecraft.cpp
@@ -1,10 +1,5 @@
 #include<iostream>
 using namespace std;
 main(){ 
-cout<<"      /\\    "<<endl;
-cout<<"     /~~\\   "<<endl;
-cout<<"   __     __  "<<endl;
-cout<<"  /  |  |   \\ "<<endl;
-cout<<"======.  .===== "<<endl;
-cout<<"      ||||       "<<endl;
+	cout << "Hello World;"
 }
